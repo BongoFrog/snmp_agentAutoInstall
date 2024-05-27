ebc976e5e5b5b16ce807dadcd1f93e4929f1c66f
UIT.19522399 <74302976+trieulam15@users.noreply.github.com>
2024-05-27 18:40:56 +0700

    Update autoinstall2.sh

--- autoinstall2.sh
+++ autoinstall2.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 install_net_snmp() {
@@ -20,7 +21,8 @@ install_net_snmp() {
 
 # Function to change SNMP configuration file
 change_snmp_config() {
-    sudo sed -i 's/^com2sec notConfigUser  default       public/com2sec notConfigUser  default       your_community_string/' /etc/snmp/snmpd.conf
+    community_string=$1
+    sudo sed -i "s/^com2sec notConfigUser  default       public/com2sec notConfigUser  default       $community_string/" /etc/snmp/snmpd.conf
     sudo sed -i '/rocommunity public  default    -V systemonly/d' /etc/snmp/snmpd.conf
     sudo systemctl restart snmpd
 }
@@ -28,13 +30,14 @@ change_snmp_config() {
 # Function to change firewall rules to allow access from monitor host
 change_firewall_rules() {
     os=$(cat /etc/os-release | grep "^ID=" | cut -d'=' -f2 | tr -d '"')
+    monitor_host_ip=$1
     case $os in
         ubuntu)
-            sudo ufw allow from monitor_host_ip to any port 161
+            sudo ufw allow from $monitor_host_ip to any port 161
             sudo ufw reload
             ;;
         centos | rhel)
-            sudo firewall-cmd --zone=public --add-rich-rule='rule family="ipv4" source address="monitor_host_ip" port protocol="udp" port="161" accept'
+            sudo firewall-cmd --zone=public --add-rich-rule='rule family="ipv4" source address="$monitor_host_ip" port protocol="udp" port="161" accept'
             sudo firewall-cmd --reload
             ;;
         *)
@@ -46,7 +49,7 @@ change_firewall_rules() {
 
 # Main script
 install_net_snmp
-change_snmp_config
-change_firewall_rules
+change_snmp_config "your_community_string"
+change_firewall_rules "monitor_host_ip"
 
 echo "Auto installation and configuration completed successfully."
