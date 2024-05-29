bf070ceb044e6c5deb25d0fbaef2ee7d4e6eb51d
UIT.19522399 <74302976+trieulam15@users.noreply.github.com>
2024-05-29 08:50:27 +0700

    Update autoinstall2.sh
    change from using case to else if case and detect os using install packages

--- autoinstall2.sh
+++ autoinstall2.sh
@@ -1,55 +1,42 @@
 #!/bin/bash
 
 install_net_snmp() {
-    os=$(cat /etc/os-release | grep "^ID=" | cut -d'=' -f2 | tr -d '"')
-    case $os in
-        ubuntu)
-            sudo apt update
-            sudo apt install snmpd
-            ;;
-        centos | rhel)
-            sudo yum update
-            sudo yum install snmpd
-            ;;
-        *)
-            echo "Unsupported OS"
-            exit 1
-            ;;
-    esac
+    if command -v apt > /dev/null; then
+        sudo apt update
+        sudo apt install -y net-snmp
+    elif command -v yum > /dev/null; then
+        sudo yum update
+        sudo yum install -y net-snmp
+    else
+        echo "Unsupported package manager"
+        exit 1
+    fi
 }
 
 # Function to change SNMP configuration file
 change_snmp_config() {
-    community_string=$1
-    sudo sed -i "s/^com2sec notConfigUser  default       public/com2sec notConfigUser  default       $community_string/" /etc/snmp/snmp.conf
-    sudo sed -i '/rocommunity public  default    -V systemonly/d' /etc/snmp/snmp.conf
+    sudo sed -i 's/^com2sec notConfigUser  default       public/com2sec notConfigUser  default       your_community_string/' /etc/snmp/snmpd.conf
+    sudo sed -i '/rocommunity public  default    -V systemonly/d' /etc/snmp/snmpd.conf
     sudo systemctl restart snmpd
 }
 
 # Function to change firewall rules to allow access from monitor host
 change_firewall_rules() {
-    os=$(cat /etc/os-release | grep "^ID=" | cut -d'=' -f2 | tr -d '"')
-    monitor_host_ip=$1
-    case $os in
-        ubuntu)
-	    sudo ufw enable
-            sudo ufw allow from $monitor_host_ip to any port 161
-            sudo ufw reload
-            ;;
-        centos | rhel)
-            sudo firewall-cmd --zone=public --add-rich-rule='rule family="ipv4" source address="$monitor_host_ip" port protocol="udp" port="161" accept'
-            sudo firewall-cmd --reload
-            ;;
-        *)
-            echo "Unsupported OS"
-            exit 1
-            ;;
-    esac
+    if command -v ufw > /dev/null; then
+        sudo ufw allow from monitor_host_ip to any port 161
+        sudo ufw reload
+    elif command -v firewall-cmd > /dev/null; then
+        sudo firewall-cmd --zone=public --add-rich-rule='rule family="ipv4" source address="monitor_host_ip" port protocol="udp" port="161" accept'
+        sudo firewall-cmd --reload
+    else
+        echo "Unsupported firewall configuration tool"
+        exit 1
+    fi
 }
 
 # Main script
 install_net_snmp
-change_snmp_config "your_community_string"
-change_firewall_rules "monitor_host_ip"
+change_snmp_config
+change_firewall_rules
 
 echo "Auto installation and configuration completed successfully."
